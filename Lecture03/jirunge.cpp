fac331cc4e23072ab1f1a7c49683bcc8094e5015
LYS924 <qbdtlstorl123@naver.com>
2023-04-16 19:45:15 +0900

    *지렁이 게임 스테이지 완성

--- Lecture03/jirunge.cpp
+++ Lecture03/jirunge.cpp
@@ -1,8 +1,4 @@
-﻿// jirunge.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <iostream>
-#include <stdio.h>
+﻿#include <iostream>
 #include <conio.h>
 #include <Windows.h>
 
@@ -35,98 +31,94 @@ int print_introduction_screen()
     std::cout << "**************************************************************" << std::endl;
     return 0;
 }
-void gotoxy(int x, int y)
-{
-    COORD pos = { x, y };
+void gotoxy(int x, int y) {
+    COORD pos = { x,y };
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 int print_game_screen(int stage_width, int stage_height)
 {
-    gotoxy(0, 0);
-    std::cout << "**************************************************************" << std::endl;
-    std::cout << "*                                                            *" << std::endl;
-    std::cout << "*                                                            *" << std::endl;
-    std::cout << "*                                                            *" << std::endl;
-    std::cout << "*                                                            *" << std::endl;
-    std::cout << "*                                                            *" << std::endl;
-    std::cout << "*                                                            *" << std::endl;
-    std::cout << "*                                                            *" << std::endl;
-    std::cout << "*                                                            *" << std::endl;
-    std::cout << "*                                                            *" << std::endl;
-    std::cout << "*                                                            *" << std::endl;
-    std::cout << "*                                                            *" << std::endl;
-    std::cout << "*                                                            *" << std::endl;
-    std::cout << "*                                                            *" << std::endl;
-    std::cout << "**************************************************************" << std::endl;
-
+    system("cls");
+    for (int i = 0; i <= stage_width + 1; i++)
+    {
+        gotoxy(i, 0);
+        printf("*");
+    }
+    for (int i = 1; i <= stage_height; i++)
+    {
+        gotoxy(0, i);
+        printf("*");
+        gotoxy(stage_width + 1, i);
+        printf("*");
+    }
+    for (int i = 0; i <= stage_width + 1; i++)
+    {
+        gotoxy(i, stage_height + 1);
+        printf("*");
+    }
     return 0;
-    
 }
-
 int main()
 {
-    int gamerunning = 1;
-    int gamemode = 0;
-
-    while (gamerunning)
+    int game_state = 0;
+    int is_game_running = 1;
+    while (is_game_running)
     {
-        switch (gamemode)
+        char key_input = '0';
+        switch (game_state)
         {
         case 0:
             print_title_screen();
-
-            char button;
-
-            do
+            while (key_input != '1' && key_input != '2' && key_input != '4' && key_input != 27)
             {
-                button = _getch();
-            } while (button != '1' && button != '2' && button != '4' && button != 27);
-            if (button == '1')
-            {
-                gamemode = 1;
-
+                key_input = _getch();
             }
-            else if (button == '2')
+            switch (key_input)
             {
-                gamemode = 2;
-            }
-            else if (button == '4' || button == 27)
-            {
-                gamerunning = 0;
+            case '1':
+                game_state = 1;
+                break;
+            case '2':
+                game_state = 2;
+                break;
+            case '3':
+                break;
+            case '4':
+            case 27:
+                is_game_running = 0;
+                break;
+            default:
+                break;
             }
             break;
         case 1:
-            print_game_screen(0, 0);
+            print_game_screen(20, 10);
+            key_input = _getch();
             break;
         case 2:
             print_introduction_screen();
-            button = 0;
-            do
+            while (key_input != 'y' && key_input != 'n')
             {
-                button = _getch();
-            } while (button != 'y' && button != 'n');
-            if (button == 'y')
-            {
-                gamemode = 0;
+                key_input = _getch();
             }
-            else if (button != 'n')
+            switch (key_input)
             {
+
+            case 'y':
+                game_state = 0;
+                break;
+            case 'n':
                 print_introduction_screen();
+                key_input = _getch();
+                break;
+            default:
+                break;
             }
             break;
         default:
             break;
         }
+
+
     }
+    return 0;
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
